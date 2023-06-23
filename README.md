6d075a0a4424b2e6e0f1547cddb23d2612fde717
Paul Kimball <pekimball@verizon.net>
2023-06-23 14:58:16 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # spring-web-example
+
+## Important Spring Annotations:
+
+@SpringBootApplication
+
+@GetMapping
+@PostMapping
+@PutMapping
+@DeleteMapping
+Annotate Java methods
+Specify the "path" part of the URL to which the method responds
+
+{name}
+==
+@RequestMapping  (all of the above)
+Can also annotate a controller class to set a base path for the URL
+
+
+@RestController
+
+Annotates a class
+Marks a class that provides RESTful web content
+
+@PathParameter
+
+Annotates a method parameter
+maps a portion of the path {name} to a variable
--- src/main/java/org/yearup/springwebexample/DuckController.java
+++ src/main/java/org/yearup/springwebexample/DuckController.java
@@ -1,32 +1,34 @@
 package org.yearup.springwebexample;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/ducks")
 public class DuckController {
 
-    List<Duck> ducks = new ArrayList<>();
+    Map<String, Duck> ducks = new HashMap();
 
     DuckController(){
-        ducks.add(new Duck("Benny", false, 67.0));
-        ducks.add(new Duck("Patsy", true, 23.0));
-        ducks.add(new Duck("Penny", false, 47.0));
+        ducks.put("Benny", new Duck("Benny", false, 67.0));
+        ducks.put("Patsy", new Duck("Patsy", true, 23.0));
+        ducks.put("Penny", new Duck("Penny", false, 47.0));
     }
 
-    @GetMapping("/ducks/penny")
-    Duck getMyDuck(){
-        return ducks.get(2);
+    @GetMapping("/{name}")
+
+    Duck getMyDuck( @PathVariable("name") String name){
+        return ducks.get(name);
 
     }
 
-    @GetMapping("/ducks")
+    @GetMapping("")
     List<Duck> allMyDucks(){
-        return ducks;
+        return ducks.values().stream().toList();
     }
 
 }
